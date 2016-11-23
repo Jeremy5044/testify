a843c6c996761c2c9c43c828825dc4b4f1aee53f
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-11-23 10:37:55 -0500

    push to git

--- src/js/hard.js
+++ src/js/hard.js
@@ -5,57 +5,79 @@ class GildedRose {
     this.quality = quality;
   }
 
+
+
+
+  
+ 
+
+  
   tick () {
-    if (this.name.includes("Conjured")) {
-      this.quality = this.quality - 2;
-        
-
+    if (this.name === "Aged Brie"){
+      this.agedBrie();
+    } else if (this.name ==="Sulfuras, Hand of Ragnaros"){
+      this.Sulfuras()
+    } else if (this.name ==="Conjured Mana Cake"){
+      this.conjured();
+    } else if (this.name === "Backstage passes to a TAFKAL80ETC concert"){
+      this.backstage();
     } else {
-
-      if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.quality > 0) {
-          if (this.name != 'Sulfuras, Hand of Ragnaros') {
-            this.quality = this.quality - 1;
-          }
-        }
-      } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1;
-          if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.sellIn < 11) {
-              if (this.quality < 50) {
-                this.quality = this.quality + 1;
-              }
-            }
-            if (this.sellIn < 6) {
-              if (this.quality < 50) {
-                this.quality = this.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.name != 'Sulfuras, Hand of Ragnaros') {
-        this.sellIn = this.sellIn - 1;
-      }
-      if (this.sellIn < 0) {
-        if (this.name != 'Aged Brie') {
-          if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.quality > 0) {
-              if (this.name != 'Sulfuras, Hand of Ragnaros') {
-                this.quality = this.quality - 1;
-              }
-            }
-          } else {
-            this.quality = this.quality - this.quality;
-          }
-        } else {
-          if (this.quality < 50) {
-            this.quality = this.quality + 1;
-          }
-        }
-      }
+      this.normal();
     }
+
+  }
+  normal () {
+    this.sellIn--;
+    if (this.sellIn > 0) {
+      this.quality = this.quality - 1;
+    } else {
+      this.quality = this.quility - 2;
+    }
+    if (this.quality < 0){
+      this.quality = 0;
+    }
+
+  }
+
+conjured () {
+      this.sellIn--;
+    
+    if (this.sellIn >= 0) {
+        this.quality = this.quality -= 2;
+    } else { 
+        this.sellIn < 0
+        this.quality = this.quality -= 4;
+   }
+    if (this.quality < 0) {
+        this.quality = 0;
+    }
+  }
+    
+
+
+
+
+agedBrie () {
+    this.sellIn--;
+    
+    if (this.sellIn > 0) {
+        this.quality = this.quality += 1;
+    
+    } else if (this.sellIn <= 0){
+      this.quality = this.quility += 2;
+    
+    } if (this.quality > 50) {
+       this.quality = 50;
+    }
+  }
+Backstage () {
+    this.sellIn--;
+
+    
+    
+  }
+
+  Sulfuras () {
   }
 }
 
